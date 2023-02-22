291530706192ad3886eaa3ca784389182d2de610
gbfranko <107503088+gbfranko@users.noreply.github.com>
2023-02-22 00:49:49 -0500

    Update Part 2a

---
+++ Part 2/Part2a.py
@@ -0,0 +1,24 @@
+import findspark
+findspark.init('/home/ubuntu/spark-3.3.1-bin-hadoop3')
+
+findspark.find()
+
+from pyspark.sql import SparkSession
+
+# The entry point into all functionality in Spark is the SparkSession class.
+spark = (SparkSession
+         .builder
+         .appName("Data Sorter")
+         .master("spark://172.31.47.167:7077")
+         .getOrCreate())
+
+# Read the file in
+df = spark.read.load("hdfs://172.31.47.167:9000/export.csv", format = 'csv', inferSchema = 'true', sep = ',', header='true')
+
+#Sort the dataset, first by cca2 then by the timestamp
+df1 = df.sort('cca2', 'timestamp')
+
+#Saves the sorted file as a csv
+df1.write.format("csv").save("hdfs://172.31.47.167:9000/new")
+
+spark.stop()
